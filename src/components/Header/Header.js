d22130f6af36b4768cd1d669758a7243b628323c
Marcus Anderberg <marcusanderberg@gmail.com>
2019-02-19 18:17:49 -0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Header.css';
+import '../App/App';
+
+class Header extends Component {
+    render() {
+        return (
+
+            <header className="App-header">
+               
+                <h1>React Actors</h1>
+
+            </header>
+
+        );
+    }
+}
+
+export default Header;
